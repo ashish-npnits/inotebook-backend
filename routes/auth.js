99974b4da258b2fcfa60da22463e5cb644bd0ae7
Ashish Kumar <ashish@npnits.com>
2023-09-15 18:32:52 +0530

    added express-validator and validation logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 router.get('/', (req, res)=>{
     obj = {
@@ -10,11 +11,26 @@ router.get('/', (req, res)=>{
     res.json(obj)
 } )
 
-router.post('/', (req, res)=>{
+router.post('/',[
+    body('name', 'Enter a valid name').isLength({min: 3}),
+    body('email', 'Enter a valid name').isEmail(),
+    body('password').isLength({min: 5})
+], (req, res)=>{
+    const error = validationResult(req)
+    if(!error.isEmpty()){
+        return res.status(400).json({errors: error.array()});
+    }
     console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.json(req.body)
+    const user = User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(
+        user => res.json(user)
+    ).catch(
+        err => res.json({error: 'Please enter uniques value for the email', message: err.message})
+    )
+    
 } )
 
 module.exports = router
