e30bee090e7f80d46e1848cffc5ca826f457dbc0
Ashish Kumar <ashish@npnits.com>
2023-09-17 16:15:45 +0530

    update notes functionality

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -8,7 +8,7 @@ const fetchuser =(req, res, next) =>{
     }
     try {
         const data = jwt.verify(token,JWT_SECRET);
-        console.log(data);
+        
         req.userid = data.id;
         next();
     } catch (error) {
--- routes/notes.js
+++ routes/notes.js
@@ -24,4 +24,24 @@ router.post('/createnotes',fetchuser,[
     res.json(savedNotes);
 } )
 
+router.put('/updatenotes/:id',fetchuser, async (req, res)=>{
+    
+
+    const {title, description, tag} = req.body;
+    let newNote = {};
+    if(title){newNote.title = title}
+    if(description){newNote.description = description}
+    if(tag){newNote.tag = tag}
+
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+    console.log("note.user.toString "+note.user.toString());
+    console.log("req.userid "+req.userid);
+    if(note.user.toString() !== req.userid){return res.status(401).send("Not Allowed")}
+
+     note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    
+    res.json(note);
+} )
+
 module.exports = router
